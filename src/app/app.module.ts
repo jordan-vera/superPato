f4a3adea3a78ff3a626d12bb407c5d0db9949c34
Jordan Vera <jordantonio.vera@gmail.com>
2022-02-01 14:24:10 -0500

    jhtfy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,14 +27,10 @@ import { ProfileComponent } from "./views/profile/profile.component";
 
 import { AdminNavbarComponent } from "./components/navbars/admin-navbar/admin-navbar.component";
 import { AuthNavbarComponent } from "./components/navbars/auth-navbar/auth-navbar.component";
-import { CardBarChartComponent } from "./components/cards/card-bar-chart/card-bar-chart.component";
-import { CardLineChartComponent } from "./components/cards/card-line-chart/card-line-chart.component";
 import { CardPageVisitsComponent } from "./components/cards/card-page-visits/card-page-visits.component";
 import { CardProfileComponent } from "./components/cards/card-profile/card-profile.component";
 import { CardSettingsComponent } from "./components/cards/card-settings/card-settings.component";
 import { CardSocialTrafficComponent } from "./components/cards/card-social-traffic/card-social-traffic.component";
-import { CardStatsComponent } from "./components/cards/card-stats/card-stats.component";
-import { CardTableComponent } from "./components/cards/card-table/card-table.component";
 import { FooterAdminComponent } from "./components/footers/footer-admin/footer-admin.component";
 import { FooterComponent } from "./components/footers/footer/footer.component";
 import { FooterSmallComponent } from "./components/footers/footer-small/footer-small.component";
@@ -42,11 +38,7 @@ import { HeaderStatsComponent } from "./components/headers/header-stats/header-s
 import { IndexNavbarComponent } from "./components/navbars/index-navbar/index-navbar.component";
 import { MapExampleComponent } from "./components/maps/map-example/map-example.component";
 import { IndexDropdownComponent } from "./components/dropdowns/index-dropdown/index-dropdown.component";
-import { TableDropdownComponent } from "./components/dropdowns/table-dropdown/table-dropdown.component";
-import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pages-dropdown.component";
-import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
 
 import { ProductosComponent } from './views/admin/productos/productos.component';
 import { ClientesComponent } from './views/admin/clientes/clientes.component';
@@ -59,13 +51,7 @@ import { MiperfilComponent } from './views/admin/miperfil/miperfil.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    CardBarChartComponent,
-    CardLineChartComponent,
     IndexDropdownComponent,
-    PagesDropdownComponent,
-    TableDropdownComponent,
-    NotificationDropdownComponent,
-    UserDropdownComponent,
     SidebarComponent,
     FooterComponent,
     FooterSmallComponent,
@@ -74,8 +60,6 @@ import { MiperfilComponent } from './views/admin/miperfil/miperfil.component';
     CardProfileComponent,
     CardSettingsComponent,
     CardSocialTrafficComponent,
-    CardStatsComponent,
-    CardTableComponent,
     HeaderStatsComponent,
     MapExampleComponent,
     AuthNavbarComponent,
